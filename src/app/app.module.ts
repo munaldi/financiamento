8710380066e6a320526add10f1c20d30e7347655
NezioMunaldi <munaldi@gmail.com>
2020-07-25 22:45:21 -0300

    carga inicial do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { PlotsComponent } from './plots/plots.component';
+import { DropdownDirective } from "./shared/dropdown.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    PlotsComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+
+export class BodyComponent implements OnInit {
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+
+
+}
